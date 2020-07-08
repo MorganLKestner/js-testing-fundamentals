becbe3cf1ac1ef285a0465ccbde4d5d7bab1e63b
Morgan Lee Kestner <morgankestner@Morgans-MacBook-Air.local>
2020-07-08 19:16:31 -0400

    video 1

--- lessons/simple.js
+++ lessons/simple.js
@@ -1,15 +1,10 @@
+
 const {sum, subtract} = require('../math')
 
-let result, expected
+let result = sum(3,7)
+let expected = 10;
 
-result = sum(3, 7)
-expected = 10
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
-}
-
-result = subtract(7, 3)
-expected = 4
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+result = subtract(7,3)
+if(result !== expected) {
+  throw new Error(`${result} is not equal to  ${expected}`)
 }
