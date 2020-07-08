4c294743bbf611d4003b9aca27fb969ee9a5cbcd
Morgan Lee Kestner <morgankestner@Morgans-MacBook-Air.local>
2020-07-08 19:52:25 -0400

    video 6

--- lessons/jest.test.js
+++ lessons/jest.test.js
@@ -1,38 +1,14 @@
 const {sumAsync, subtractAsync} = require('../math')
 
-test("sum adds numbers", async () => {
-  const result = await sumAsync(3, 7);
-  const expected = 10;
-  expect(result).toBe(expected);
-});
+test('sumAsync adds numbers asynchronously', async () => {
+  const result = await sumAsync(3, 7)
+  const expected = 10
+  expect(result).toBe(expected)
+})
 
-
-test("sum subtracts numbers", async () => {
-  const result = await sumAsync(7, 3);
-  const expected = 4;
-  expect(result).toBe(expected);
-});
-
-
-async function test(title, callback) {
-  try {
-    await callback()
-    console.log(`^ ${title}`)
-  } catch (error) {
-    console.error(`x ${title}`)
-    console.error(error)
-  }
- }
-
-// test('sumAsync adds numbers asynchronously', async () => {
-//   const result = await sumAsync(3, 7)
-//   const expected = 10
-//   expect(result).toBe(expected)
-// })
-
-// test('subtractAsync subtracts numbers asynchronously', async () => {
-//   const result = await subtractAsync(7, 3)
-//   const expected = 4
-//   expect(result).toBe(expected)
-// })
+test('subtractAsync subtracts numbers asynchronously', async () => {
+  const result = await subtractAsync(7, 3)
+  const expected = 4
+  expect(result).toBe(expected)
+})
  
